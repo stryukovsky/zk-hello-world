46bfbe8cb764f6ad9059e5eefb4b4dac00e3b6db
Dmitry Stryukovsky <strukovsky1@gmail.com>
2023-03-13 18:04:22 +0400

    feat: setup project

---
+++ README.md
@@ -0,0 +1,10 @@
+# ZkSync era study
+
+Start yarn project
+```
+yarn
+```
+Compile contract
+```
+yarn hardhat compile
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import { config } from "dotenv";
+config();
+
+module.exports = {
+  zksolc: {
+    version: "1.3.1",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
+
+  networks: {
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: process.env.GOERLI_URL, // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+  },
+};
